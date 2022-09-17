005500ab72cb04c58e2e6419be8b3db444a69860
JANEROSE <janerosenyams@gmail.com>
2022-09-17 21:24:25 +0300

    Added co:here classifier and embedder

---
+++ models/svm_classifier.py
@@ -0,0 +1,18 @@
+from sklearn.svm import SVC
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+
+
+class SVMClassifier():
+    """ Initialize a support vector machine, with class_weight='balanced' because 
+    our training set has roughly an equal amount of positive and negative 
+    sentiment sentences"""
+    def __init__(self, embed_train_data, test_data) -> None:
+        self.embed_training_data = embed_train_data
+        self.testing_data = test_data
+
+    def classification_model(self):
+        svm_classifier = make_pipeline(StandardScaler(), SVC(class_weight='balanced')) 
+        # fit the support vector machine
+        svm_classifier.fit(self.embed_train_data, self.test_data)
+        return svm_classifier
